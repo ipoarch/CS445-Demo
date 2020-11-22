af936f6b8912a709ac6283fbcd39f702c665bfc9
Isaac Poarch <sean.poarch@duke.edu>
2020-11-21 21:46:15 -0500

    tried L1 and added the basis color

--- image_importer.py
+++ image_importer.py
@@ -7,7 +7,7 @@ def decompose_img(filename):
         # print(picture_rgb)
         return np.array([picture_rgb, f.size[0], f.size[1]], dtype='object')
 
-def make_image(filename, W, H, width, height, mode="reconstructed"):
+def make_image(filename, W, H, width, height, mode="reconstructed", l1r=False):
     if (mode == "parts"):
         new_pixels = np.empty([1, 3])
         count = 0
@@ -19,8 +19,24 @@ def make_image(filename, W, H, width, height, mode="reconstructed"):
                 new_pixels = np.append(new_pixels, [np.array([250, 0, 0])], axis=0)
         new_pixels = new_pixels[1:].astype('i')
         itr = tuple(map(tuple, new_pixels))
+    elif (mode == "parts2"):
+        new_pixels = np.empty([1, 3])
+        count = 0
+        for w in W:
+            count = count + 1
+            if (w[0] > w[1]):
+                new_pixels = np.append(new_pixels, [H[0] * 250 / max(H[0])], axis=0)
+            else:
+                new_pixels = np.append(new_pixels, [H[1] * 250 / max(H[1])], axis=0)
+        new_pixels = new_pixels[1:].astype('i')
+        itr = tuple(map(tuple, new_pixels))
     else:
         itr = tuple(map(tuple, np.matmul(W, H).astype('i')))
+
+    if l1r:
+        prefix = "l1"
+    else:
+        prefix = ""
 
     # print(len(new_pixels))
     # print(width * height)
@@ -37,4 +53,4 @@ def make_image(filename, W, H, width, height, mode="reconstructed"):
         # f.show()
         f = f.rotate(-90, expand=True)
         f = ImageOps.mirror(f)
-        f.save("generated/" + mode + "_" + filename, "JPEG")
+        f.save("generated/" + prefix + "_" + mode + "_" + filename, "JPEG")
--- main.py
+++ main.py
@@ -12,15 +12,18 @@ for filename in os.listdir('european-flood-2013_imgs_small/imgs_small/')[:]:
     every_pixel = np.append(every_pixel, decomp[0], axis=0)
 
     model = NMF(n_components=2, init='random', random_state=0)
+    # model = NMF(n_components=2, init='random', random_state=0, l1_ratio=1)
+    l1 = False
     W = model.fit_transform(every_pixel[1:])
     H = model.components_
 
-    # print(W)
-    # print(H)
+    print(W)
+    print(H)
     # print(decomp[0])
     # print(decomp[1])
     # print(decomp[2])
     # print(filename)
 
-    i.make_image(filename, W, H, decomp[1], decomp[2], mode="parts")
-    i.make_image(filename, W, H, decomp[1], decomp[2])
+    # i.make_image(filename, W, H, decomp[1], decomp[2], mode="parts", l1r=l1)
+    i.make_image(filename, W, H, decomp[1], decomp[2], mode="parts2", l1r=l1)
+    # i.make_image(filename, W, H, decomp[1], decomp[2], l1r=l1)
